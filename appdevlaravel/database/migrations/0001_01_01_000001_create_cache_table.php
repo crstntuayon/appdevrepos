6af18352098beb9b93614dcae1e6e0827c29faa0
crese <crestuayon@gmail.com>
2025-04-03 21:50:26 -0700

    update: migrations

--- appdevlaravel/database/migrations/0001_01_01_000001_create_cache_table.php
+++ appdevlaravel/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,16 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
-
-        Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->string('owner');
-            $table->integer('expiration');
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -29,7 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
