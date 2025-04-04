b8911b66dd1ac8579ff8dd606a660172a9c4396e
crese <crestuayon@gmail.com>
2025-04-03 21:31:31 -0700

    update: web.php

--- appdevlaravel/routes/web.php
+++ appdevlaravel/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('', [HomeController::class, 'index'])->name('student.index');
+Route::get('/create', [HomeController::class, 'create'])->name('student.create');
+Route::post('/create', [HomeController::class, 'store'])->name('student.store');
+Route::get('/edit/{student}', [HomeController::class, 'edit'])->name('student.edit');
+Route::put('/update/{student}', [HomeController::class, 'update'])->name('student.update');
+Route::get('/destroy/{student}', [HomeController::class, 'destroy'])->name('student.destroy');
